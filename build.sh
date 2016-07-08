1a4b7520397544f5c5a649589a4c210e0bed6f7b
Bruce Becker <brucellino@gmail.com>
2016-07-08 11:05:07 +0200

    Different download path. Fixed #1 Fixed  #2 Fixed  #3 Fixed #4 Fixed #5 Fixed  #6 Fixed #7 Fixed #8 Fixed  #9 Fixed   #10 Fixed  #11 Fixed 12

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ mkdir -p ${SOFT_DIR}
 if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
   echo "seems like this is the first build - let's geet the source"
-  wget http://prdownloads.sourceforge.net/${NAME}/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
+  wget http://downloads.sourceforge.net/project/${NAME}/${NAME}-${VERSION}/${VERSION/}${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
   echo "releasing lock"
   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
 elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
